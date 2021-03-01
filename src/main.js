72d6cc8f04b16eb3ff0aff97f23121584417947e
Alexandre MEYER <alexandre.meyer@iconsultants.fr>
2021-03-01 21:53:43 +0100

    displaying btc usdt price

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  // store,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
