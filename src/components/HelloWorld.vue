cb10de9ff97893905ace95fc6e501cb2aefe329d
Alexandre MEYER <alexandre.meyer@iconsultants.fr>
2021-03-03 22:54:52 +0100

    play that roar sound

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <v-row class="text-center">
-      btc usd price : {{this.price}}
+      <h1>btc usd price : {{this.price}}</h1>
     </v-row>
   </v-container>
 </template>
@@ -18,13 +18,32 @@ export default {
     }    
   },
   mounted: function() {
-    socketTrade.onmessage = function(event) {      
+    socketTrade.onmessage = function(event) {
       const binanceData = JSON.parse(event.data)
       this.price = binanceData.p
     }.bind(this)
+  }, 
+  watch: {
+    // whenever price changes, this function will run
+    price: function (newPrice) {
+      if (parseInt(newPrice) > 50450) {
+        this.playAudio()
+        console.log("toto")
+        this.stopAudio()
+      } else {
+        console.log("tata")
+      }
+    }
   },
   methods: {
-
+    playAudio() {
+      const audio = new Audio('https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3');
+      audio.play();
+    },
+    stopAudio() {
+      const audio = new Audio('https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3');
+      audio.pause();
+    }
   }      
 }
 </script>
